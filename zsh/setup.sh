dd7c0829072d9fa448698bbb124fcbe1c4300502
MaxaiZer <max7zer@gmail.com>
2025-01-19 06:38:58 -0500

    change zsh config and setup script

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,20 +3,41 @@
 set -e
 
 echo "Installing Zsh..."
-sudo dnf update
 sudo dnf install zsh -y
 
 echo "Setting Zsh as the default shell..."
 chsh -s "$(which zsh)"
 
-echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Custom: $ZSH_CUSTOM"
 
-echo "Installing zsh-autosuggestions..."
-git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "Oh My Zsh is already installed."
+else
+    echo "Installing Oh My Zsh..." 
+    sh -x -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
-echo "Installing zsh-syntax-highlighting..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+if [ -z "$ZSH_CUSTOM" ]; then
+	ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"	
+fi
 
-echo "Installing Powerlevel10k theme..."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
+if [ -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    echo "zsh-autosuggestions are already installed."
+else
+    echo "Installing zsh-autosuggestions..."
+    sudo git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+fi
+
+if [ -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    echo "zsh-syntax-highlighting is already installed."
+else
+    echo "Installing zsh-syntax-highlighting..."
+    sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+fi
+
+if [ -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+    echo "powerlevel10k is already installed."
+else
+    echo "Installing Powerlevel10k theme..."
+    sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
+fi
