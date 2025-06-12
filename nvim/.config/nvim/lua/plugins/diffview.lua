5c23d73666b2d9b5ba98ec6a17b78695a4cf560a
MaxaiZer <max7zer@gmail.com>
2025-06-12 17:02:10 +0700

    add shortcut for diffview

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -2,4 +2,18 @@ return {
   "sindrets/diffview.nvim",
   event = "VeryLazy",
   dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    {
+      "dv",
+      function()
+        if next(require("diffview.lib").views) == nil then
+          vim.cmd("DiffviewOpen")
+        else
+          vim.cmd("DiffviewClose")
+        end
+      end,
+      desc = "Toggle Diffview window",
+      mode = "n",
+    },
+  },
 }
