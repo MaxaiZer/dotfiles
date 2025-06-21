66d9b1caa88962acf55507f45961a401f8bde9ff
MaxaiZer <max7zer@gmail.com>
2025-06-21 14:35:37 +0700

    make lazygit fullscreen

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -6,4 +6,8 @@ return {
   keys = {
     { "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" },
   },
+  init = function()
+    vim.g.lazygit_floating_window_use_plenary = 1
+    vim.g.lazygit_floating_window_scaling_factor = 1.0
+  end,
 }
