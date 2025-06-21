07bafddebb4bc31ee2de45654e92b27f2e467eb9
MaxaiZer <max7zer@gmail.com>
2025-06-21 14:11:26 +0700

    add keymaps for quit and select all

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,2 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.keymap.set("n", "<C-a>", "gg0vG$", { desc = "Select all" })
+vim.keymap.set("n", "<C-q>", ":qa<CR>", { desc = "Quit all" })
