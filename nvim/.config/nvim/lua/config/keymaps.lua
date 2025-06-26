dfe6bc0f99269b107bd591e37aa75f304f981f28
MaxaiZer <max7zer@gmail.com>
2025-06-27 02:15:44 +0700

    fix live grep window size + proportions

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,15 @@
 vim.keymap.set("n", "<C-a>", "gg0vG$", { desc = "Select all" })
 vim.keymap.set("n", "<C-q>", ":qa<CR>", { desc = "Quit all" })
+
+vim.keymap.set("n", "<Space>/", function()
+  require("telescope.builtin").live_grep({
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        prompt_position = "bottom",
+        results_width = 0.35,
+        preview_width = 0.65,
+      },
+    },
+  })
+end, { desc = "Live Grep" })
