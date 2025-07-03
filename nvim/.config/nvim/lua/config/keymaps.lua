d20ee525223de76f3c5b3e42261add7121bc8f81
MaxaiZer <max7zer@gmail.com>
2025-07-03 20:52:41 +0700

    add keymap to toggle inlay hints

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -35,3 +35,10 @@ vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "Neogit" })
 vim.keymap.set("n", "<leader>gB", function()
   require("gitsigns").toggle_current_line_blame()
 end, { desc = "Toggle GitSigns current line blame" })
+
+vim.keymap.set("n", "<leader>uh", function()
+  local ih = vim.lsp.inlay_hint or vim.lsp.buf.inlay_hint
+  local bufnr = vim.api.nvim_get_current_buf()
+  local enabled = ih.is_enabled({ bufnr = bufnr })
+  ih.enable(not enabled, { bufnr = bufnr })
+end, { desc = "Toggle Inlay Hints" })
