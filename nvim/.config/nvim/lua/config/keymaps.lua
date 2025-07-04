9195f0457ab02f7e61fe35490a6d30c8da5ba6b7
MaxaiZer <max7zer@gmail.com>
2025-07-04 15:53:35 +0700

    move telescope keymaps to the plugin file to prevent overrides by other plugins

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,31 +5,6 @@ vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<M-Right>", ":bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<M-Left>", ":bprevious<CR>", { desc = "Previous buffer" })
 
-vim.keymap.set("n", "<Space>/", function()
-  require("telescope").extensions.live_grep_args.live_grep_args({
-    layout_strategy = "vertical",
-    path_display = { "smart" },
-    wrap_results = true,
-    disable_coordinates = true,
-    layout_config = {
-      horizontal = {
-        width = 9999,
-        height = 9999,
-        prompt_position = "bottom",
-        results_width = 0.35,
-        preview_width = 0.65,
-      },
-      vertical = {
-        width = 9999,
-        height = 9999,
-        preview_height = 0.4,
-        preview_cutoff = 0,
-      },
-    },
-  })
-end, { desc = "Live Grep" })
-vim.keymap.set("n", "<Space>r/", ":Telescope resume<CR>", { desc = "Telescope resume" })
-
 vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "Neogit" })
 
 vim.keymap.set("n", "<leader>gB", function()
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -12,5 +12,41 @@ return {
         end,
       },
     },
+    keys = {
+      {
+        "<leader>/",
+        function()
+          require("telescope").extensions.live_grep_args.live_grep_args({
+            layout_strategy = "vertical",
+            path_display = { "smart" },
+            wrap_results = true,
+            disable_coordinates = true,
+            layout_config = {
+              horizontal = {
+                width = 9999,
+                height = 9999,
+                prompt_position = "bottom",
+                results_width = 0.35,
+                preview_width = 0.65,
+              },
+              vertical = {
+                width = 9999,
+                height = 9999,
+                preview_height = 0.4,
+                preview_cutoff = 0,
+              },
+            },
+          })
+        end,
+        desc = "Live Grep",
+        mode = "n",
+      },
+      {
+        "<Space>r/",
+        "<cmd>Telescope resume<cr>",
+        desc = "Telescope resume",
+        mode = "n",
+      },
+    },
   },
 }
