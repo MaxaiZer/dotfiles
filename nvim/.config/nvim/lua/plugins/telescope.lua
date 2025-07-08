51493988ce09190ab07f188f24f0ce5f1483b65c
MaxaiZer <max7zer@gmail.com>
2025-07-08 17:50:37 +0700

    add livegrep with clipboard

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,25 @@
+local live_grep_config = {
+  layout_strategy = "vertical",
+  path_display = { "smart" },
+  wrap_results = true,
+  disable_coordinates = true,
+  layout_config = {
+    horizontal = {
+      width = 9999,
+      height = 9999,
+      prompt_position = "bottom",
+      results_width = 0.35,
+      preview_width = 0.65,
+    },
+    vertical = {
+      width = 9999,
+      height = 9999,
+      preview_height = 0.4,
+      preview_cutoff = 0,
+    },
+  },
+}
+
 return {
   { "nvim-telescope/telescope-live-grep-args.nvim" },
   {
@@ -16,29 +38,20 @@ return {
       {
         "<leader>/",
         function()
-          require("telescope").extensions.live_grep_args.live_grep_args({
-            layout_strategy = "vertical",
-            path_display = { "smart" },
-            wrap_results = true,
-            disable_coordinates = true,
-            layout_config = {
-              horizontal = {
-                width = 9999,
-                height = 9999,
-                prompt_position = "bottom",
-                results_width = 0.35,
-                preview_width = 0.65,
-              },
-              vertical = {
-                width = 9999,
-                height = 9999,
-                preview_height = 0.4,
-                preview_cutoff = 0,
-              },
-            },
-          })
+          require("telescope").extensions.live_grep_args.live_grep_args(live_grep_config)
         end,
         desc = "Live Grep",
+        mode = "n",
+      },
+      {
+        "<leader>/c",
+        function()
+          local config = vim.tbl_deep_extend("force", live_grep_config, {
+            default_text = vim.fn.getreg("+"):match("^[^\n\r]*"),
+          })
+          require("telescope").extensions.live_grep_args.live_grep_args(config)
+        end,
+        desc = "Live Grep (clipboard)",
         mode = "n",
       },
       {
