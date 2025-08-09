bd0dde2c16fe6ff7ac772e546b2f1194c2a43918
MaxaiZer <max7zer@gmail.com>
2025-08-09 13:54:22 +0700

    add scrollbar to neovim

---
+++ nvim/.config/nvim/lua/plugins/scrollbar.lua
@@ -0,0 +1,12 @@
+return {
+  "petertriho/nvim-scrollbar",
+  opts = {
+    handlers = {
+      gitsigns = true,
+      diagnostic = true,
+      search = true,
+      ale = false,
+    },
+    excluded_filetypes = { "neo-tree" },
+  },
+}
