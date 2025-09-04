3e7315b3d55da34ee0f63849ad2a9fad5851fede
MaxaiZer <max7zer@gmail.com>
2025-09-04 20:12:36 +0700

    disable neo-tree file watcher to improve performance

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,9 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      filesystem = {
+        use_libuv_file_watcher = false,
+      },
       default_component_configs = {
         git_status = {
           symbols = {
